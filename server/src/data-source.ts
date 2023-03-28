950de23bba7af4e79739bde7c9a88362b3ba5f59
elecmjk1117 <elecmjk1117@gmail.com>
2023-03-28 12:34:16 +0900

    DB연결의 문제로 다시 커밋

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -8,7 +8,7 @@ import Vote from "./entities/Vote"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "db",
     port: 5432,
     username: "postgres",
     password: "postgres",
